822563561b2e343caefcb57935425203a074b3f2
unknown <sol.vol.rufino@gmail.com>
2020-07-21 17:39:46 -0300

    Finalizando los CallBack preparando motores para el checkPoint

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,23 +2,40 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  cb();
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  //var numeros = [1,2,3,4]
+  var sumaTotal = 0;
+  for(var i = 0; i < numeros.length; i++){
+     sumaTotal = sumaTotal + numeros[i];
+  }
+  cb(sumaTotal);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  //primer metodo mas claro es este
+  //array.forEach(function(elemento){
+  //cb(elemento);
+  //})
+  //metodo mas limpio:
+  array.forEach(cb);
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  var nuevoArray = array.map(function(elemnto) {
+    return cb(elemnto);
+  })
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
